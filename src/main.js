b7f2501b5a70e040beb9cbee9d9d6272cab6e21f
Muhammad Moeez Khan <moeezkhan010@gmail.com>
2023-12-06 02:20:19 -0500

    main: update google auth

--- src/main.js
+++ src/main.js
@@ -2,20 +2,32 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import { createRouter, createWebHistory } from 'vue-router';
-
-const pinia = createPinia();
+import '../firebase.js'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: () => import('./views/Home.vue') },
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: () => import('./views/Home.vue') },
     { path: '/sell', component: () => import('./views/Sell.vue') },
     { path: '/purchase', component: () => import('./views/Purchase.vue') },
+    { path: '/login', component: () => import('./views/Login.vue') },
   ],
 });
 
 const app = createApp(App);
-app.use(pinia);
+app.use(createPinia());
 app.use(router);
+
+onAuthStateChanged(getAuth(), (user) => {
+  if (user) {
+    router.push('/home'); // Redirect to home if user is signed in
+  } else {
+    if (router.currentRoute.value.path !== '/login') {
+      router.push('/login'); // Redirect to login if not currently on login page and no user is signed in
+    }
+  }
+});
 
 app.mount('#app');
---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login-container">
+    <button @click="loginWithGoogle" class="login-button">Login with Google</button>
+  </div>
+</template>
+
+<script>
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+export default {
+  methods: {
+    async loginWithGoogle() {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth();
+      try {
+        await signInWithPopup(auth, provider);
+      } catch (error) {
+        console.error('Google login error:', error);
+        // Optionally show an error message to the user
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Use the full height of the viewport */
+}
+
+.login-button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  /* Add any additional styling you want */
+}
+</style>
